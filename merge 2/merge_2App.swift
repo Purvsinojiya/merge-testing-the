36d15ad00de70482f40cc2e2f430ee42c6cd7cc7
Purvsinojiya <21ce135@charusat.edu.in>
2026-03-23 22:57:09 +0530

    Initial Commit

---
+++ merge 2/merge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  merge_2App.swift
+//  merge 2
+//
+//  Created by Sinojiya Purv on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct merge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
