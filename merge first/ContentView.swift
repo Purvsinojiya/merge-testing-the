a68e789775a70b9664c5748ae348303bb2522e77
Purvsinojiya <21ce135@charusat.edu.in>
2026-03-23 22:54:19 +0530

    first commit

---
+++ merge first/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  merge first
+//
+//  Created by Sinojiya Purv on 23/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("hello everyone")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ merge first/merge_firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  merge_firstApp.swift
+//  merge first
+//
+//  Created by Sinojiya Purv on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct merge_firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
